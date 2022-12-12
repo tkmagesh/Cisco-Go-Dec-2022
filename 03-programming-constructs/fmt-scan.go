300ccff8b462eb06d8f542749e416c0af029cd8b
Magesh Kuppan <tkmagesh77@gmail.com>
2022-12-12 13:41:47 +0530

    assignment-01 - problem statement

---
+++ 03-programming-constructs/fmt-scan.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	/*
+		var firstName, lastName string
+		// fmt.Println(&name)
+		fmt.Scanln(&firstName, &lastName)
+		fmt.Printf("Hi %s %s, Have a nice day!\n", firstName, lastName)
+	*/
+
+	var no int
+	fmt.Scanln(&no)
+	fmt.Println(no)
+
+}
