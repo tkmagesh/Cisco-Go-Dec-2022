fd55238adf9be6bf127db795ec280568f8e23a8c
Magesh Kuppan <tkmagesh77@gmail.com>
2022-12-12 10:26:40 +0530

    hello world in go

---
+++ 01-intro/program.go
@@ -0,0 +1,15 @@
+/* package declaration */
+package main
+
+/* import dependent packages */
+
+/* package scoped variable declarations / type definitions */
+
+/* package init function */
+
+/* main function */
+func main() {
+	print("Hello World!\n")
+}
+
+/* other functions */
