abd2a1afea564977745cc4729553634c1b284545
Magesh Kuppan <tkmagesh77@gmail.com>
2022-12-12 15:19:30 +0530

    functions as values to variables

--- 04-functions/04-fn-var.go
+++ 04-functions/04-fn-var.go
@@ -16,22 +16,28 @@ func main() {
 	greet("Magesh")
 
 	/* declare the variables for each of the below functions and use them (as above) */
-	greetMsg := func(userName string) string {
+	var greetFn func(string) string
+	greetFn = func(userName string) string {
 		var msg string
 		msg = fmt.Sprintf("Hi %q, Have a nice day", userName)
 		return msg
-	}("Magesh")
+	}
+	greetMsg := greetFn("Magesh")
 	fmt.Println(greetMsg)
 
-	result := func(x, y int) int {
+	var add func(int, int) int
+	add = func(x, y int) int {
 		return x + y
-	}(100, 200)
+	}
+	result := add(100, 200)
 	fmt.Println(result)
 
-	q, r := func(x, y int) (quotient, remainder int) {
+	var divide func(int, int) (int, int)
+	divide = func(x, y int) (quotient, remainder int) {
 		quotient = x / y
 		remainder = x % y
 		return
-	}(100, 7)
+	}
+	q, r := divide(100, 7)
 	fmt.Printf("Dividing 100 by 7, quotient = %d and remainder = %d\n", q, r)
 }
