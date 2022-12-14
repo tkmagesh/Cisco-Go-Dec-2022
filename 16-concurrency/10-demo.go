c79df60ab6984b9487002dc6c8c388fc1796a121
Magesh Kuppan <tkmagesh77@gmail.com>
2022-12-14 14:06:33 +0530

    channels introduction

---
+++ 16-concurrency/10-demo.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+//Share memory by communicating
+
+func main() {
+	/*
+		var ch chan int
+		ch = make(chan int)
+	*/
+	ch := make(chan int)
+	wg := &sync.WaitGroup{}
+	wg.Add(1)
+	go add(100, 200, wg, ch)
+	result := <-ch
+	wg.Wait()
+	fmt.Println(result)
+}
+
+func add(x, y int, wg *sync.WaitGroup, ch chan int) {
+	result := x + y
+	ch <- result
+	wg.Done()
+}
---
+++ 16-concurrency/12-demo.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+)
+
+//Share memory by communicating
+
+func main() {
+	/*
+		var ch chan int
+		ch = make(chan int)
+	*/
+	ch := make(chan int)
+	go add(100, 200, ch)
+	result := <-ch
+	fmt.Println(result)
+}
+
+func add(x, y int, ch chan int) {
+	result := x + y
+	ch <- result
+	fmt.Println("add completed")
+}
